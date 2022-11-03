6643cd89c4494305db291084c1e1ae2ead2b35d5
Sparsh <sparsh.chadha46@gmail.com>
2022-11-03 23:17:16 +0530

    made minor changes

--- app/src/main/java/com/example/retrofitinkotlin/Todo.kt
+++ app/src/main/java/com/example/retrofitinkotlin/Todo.kt
@@ -1,7 +1,7 @@
 package com.example.retrofitinkotlin
 
 data class Todo(
-//    When data is much more, instead of writing everything manually, I have installed a plugin called JSON to Kotlin converter. Click on com.example.retrofitkotlin and there click on new -> kotlin data class file from JSON. There paste the json object
+//    When data is much more, instead of writing everything manually, click on com.example.retrofitkotlin and there click on new -> kotlin data class file from JSON. There paste the json object copied from documentation
     val completed: Boolean,
     val id: Int,
     val title: String,
--- app/src/main/java/com/example/retrofitinkotlin/TodoApiInterface.kt
+++ app/src/main/java/com/example/retrofitinkotlin/TodoApiInterface.kt
@@ -3,9 +3,9 @@ package com.example.retrofitinkotlin
 import retrofit2.Response
 import retrofit2.http.GET
 
-// Here we define all the functions we need to access our API. In our case, that is just a single function to just fetch data from
+// Here we define what data we need from the API.
 interface TodoApiInterface {
-    //    In the type of response, we are passing a List of Todo because we don't just want to fetch a single object.
+    //    In the type of response, we are passing a List of Todo because we don't just want to fetch a single todo.
     @GET("/todos") // Note - in @GET we write the suffix of the base URL from which we are fetching the data. http://jsonplaceholder.typicode.com/todos
 //          this is base url                   | this is the url which we will pass in @GET
     suspend fun getTodos(): Response<List<Todo>> // Using the suspend keyword so that data is not fetched on the main thread and it is executed on coroutine
