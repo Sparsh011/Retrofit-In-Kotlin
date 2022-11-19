3e037fe11de502f97d38effecf3f27ad6bdb6581
Sparsh <sparsh.chadha46@gmail.com>
2022-11-19 23:14:48 +0530

    shimmer effect added

--- app/src/main/java/com/example/retrofitinkotlin/MainActivity.kt
+++ app/src/main/java/com/example/retrofitinkotlin/MainActivity.kt
@@ -3,11 +3,11 @@ package com.example.retrofitinkotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import androidx.core.view.isVisible
+import android.view.View
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.retrofitinkotlin.databinding.ActivityMainBinding
+import com.facebook.shimmer.ShimmerFrameLayout
 import retrofit2.HttpException
 import java.io.IOException
 
@@ -16,6 +16,8 @@ class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
     private lateinit var binding: ActivityMainBinding
     private lateinit var todoAdapter: TodoAdapter
+    private lateinit var container : ShimmerFrameLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -23,17 +25,22 @@ class MainActivity : AppCompatActivity() {
 
         setupRecyclerView()
 
+        container = findViewById(R.id.shimmer_layout)
+        container.startShimmer()
+
         lifecycleScope.launchWhenCreated {
-            binding.progressBar.isVisible = true
+
             val response = try {
                 RetrofitInstance.api.getTodos()
             } catch (e: IOException) {
                 Log.e(TAG, "IOException, You might not have internet connection! ${e.message}")
-                binding.progressBar.isVisible = false
+                container.stopShimmer()
+                container.setShimmer(null)
                 return@launchWhenCreated
             } catch (e: HttpException) {
                 Log.e(TAG, "HttpException, Unexpected Response!")
-                binding.progressBar.isVisible = false
+                container.stopShimmer()
+                container.setShimmer(null)
                 return@launchWhenCreated
             }
 
@@ -42,15 +49,15 @@ class MainActivity : AppCompatActivity() {
             } else {
                 Log.e(TAG, "Response Not successful!")
             }
-            binding.progressBar.isVisible = false
+            container.stopShimmer()
+            container.setShimmer(null)
+            container.visibility = View.GONE
         }
     }
 
     private fun setupRecyclerView() = binding.rvTodos.apply {
-//        We are using an inbuilt function here
         todoAdapter = TodoAdapter()
         adapter = todoAdapter
         layoutManager = LinearLayoutManager(this@MainActivity)
     }
-
 }
